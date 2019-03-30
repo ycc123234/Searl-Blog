2c1a20b6ec68942a87c47c6cba9402b61cdeeab0
Searl <yccddd@163.com>
2019-03-30 17:24:36 +0800

    ==

---
+++ app.js
@@ -0,0 +1,14 @@
+//add  npm module
+const express=require('express');
+const bodyParser=require('body-parser');
+
+//add routes
+var server=express();
+server.listen(8080);
+//open server
+server.use();
+
+//trusteeship static resources
+
+//mount routers
+
