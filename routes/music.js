8dd71182dd118f5f706a8020d842434a415818cb
Searl <yccddd@163.com>
2019-03-31 17:40:56 +0800

    'delete'

--- routes/music.js
+++ routes/music.js
@@ -4,7 +4,7 @@ var router=express.Router();
 
 router.get('/list',(req,res)=>{
     request('http://localhost:8888/SearlBlog/song',(err,res,body)=>{if(err)throw err;
-        console.log('statusCode',res);
+        console.log('statusCode',res&&res.statusCode);
         console.log('body:',body);
     })
 })
