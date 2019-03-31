4c5577d8678336ff57b5d3215fb071627c75f41e
Searl <yccddd@163.com>
2019-04-01 03:40:01 +0800

    promise for promise

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,47 @@
-"use strict"
-//Music加载器
-var script = document.createElement("script");
-script.type = "text/javascript";
-script.src = "/js/object/music.js";
-document.getElementsByTagName('head')[0].appendChild(script);
+// "use strict"
+var music = null;
 
-//Music执行域
-script.onload = function () {
-    var music = new Music('/music/list', 1);
-    music.init();
+function op(){
+    var isOpen=false;
+    return function(...param){
+        if(param[0]!==undefined){
+            isOpen=param[0];
+
+        }
+        return isOpen;
+    }
 }
+var op=op();
+var promise;
+function objectPro() {
+    console.log(op())
+    if(!op()){
+        promise= new Promise(function(open) {
+            function musicPro(){
+                return new Promise(function(open) {
+                    //Music加载器
+                    var script = document.createElement("script");
+                    script.type = "text/javascript";
+                    script.src = "/js/object/music.js";
+                    document.getElementsByTagName('head')[0].appendChild(script);
+        
+                    //Music执行域
+                    script.onload = function () {
+                        music = new Music('/music/list', 1);
+                        music.init();
+                        open();
+                    }
+                })
+            }
+            musicPro().then(function(){op(true);open();objectPro();});
+    
+        })
+    }else{
+        pro(promise);
+    }
+}
+
+function pro(pro){
+    pro.then(()=>{console.log(music,'########')});
+}
+objectPro();
