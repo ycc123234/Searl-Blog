edda92adfd2fd7278da31fd6b09e7fe317401cfd
Searl <yccddd@163.com>
2019-04-01 10:20:15 +0800

    update objectPro

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,43 @@
 // "use strict"
 var music = null;
+class Closer {
+    constructor(...param) {
 
-function op(){
-    var isOpen=false;
-    return function(...param){
-        if(param[0]!==undefined){
-            isOpen=param[0];
+    }
 
+    static open() {
+        var isOpen = false;
+        return function (...param) {
+            if (param[0] !== undefined) {
+                isOpen = param[0];
+
+            }
+            console.log(isOpen)
+            return isOpen;
         }
-        return isOpen;
     }
 }
-var op=op();
-var promise;
-function objectPro() {
-    console.log(op())
-    if(!op()){
-        promise= new Promise(function(open) {
-            function musicPro(){
-                return new Promise(function(open) {
+const onStart = function () {
+    var proOpen = Closer.open();
+    var promise;
+    objectPro(proOpen,promise);
+}
+
+function pro(pro) {
+    pro.then(() => { console.log(music,'########') });
+}
+//外部类加载器
+function objectPro(proOpen,promise) {
+    if (!proOpen()) {
+        promise = new Promise(function (open) {
+            function musicPro() {
+                return new Promise(function (open) {
                     //Music加载器
                     var script = document.createElement("script");
                     script.type = "text/javascript";
                     script.src = "/js/object/music.js";
                     document.getElementsByTagName('head')[0].appendChild(script);
-        
+
                     //Music执行域
                     script.onload = function () {
                         music = new Music('/music/list', 1);
@@ -33,15 +46,10 @@ function objectPro() {
                     }
                 })
             }
-            musicPro().then(function(){op(true);open();objectPro();});
-    
+            musicPro().then(function () { proOpen(true); open(); objectPro(proOpen,promise); });
+
         })
-    }else{
+    } else {
         pro(promise);
     }
 }
-
-function pro(pro){
-    pro.then(()=>{console.log(music,'########')});
-}
-objectPro();
